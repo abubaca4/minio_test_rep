ec5449c225518bcd0be19d713d87bd872dd70b31
abubaca4 <abubaca4@gmail.com>
2021-02-13 22:42:18 +0300

    coockies

--- minio_site/file_minio_storage/views.py
+++ minio_site/file_minio_storage/views.py
@@ -13,9 +13,11 @@ from minio import Minio
 
 
 def index(request):
+    num_visits=request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits+1
     return render(request,
                   'index.html',
-                  context={'num_files': File_uploaded.objects.count()},
+                  context={'num_files': File_uploaded.objects.count(), 'num_visits':num_visits},
                   )
 
 
