d63297a43ccde6b45dd1de9f31c15ddeafd5e2c7
abubaca4 <abubaca4@gmail.com>
2021-03-07 23:35:38 +0300

    file upload form

---
+++ minio_ecg/ecg_storage/static/js/file_upload.js
@@ -0,0 +1,5 @@
+var ecg_id = document.getElementById('ecg_id_field')
+var sample_frequency = document.getElementById('sample_frequency_field')
+var amplitude_resolution = document.getElementById('amplitude_resolution_field')
+var fileInput = document.getElementById('ecg_file_select')
+
