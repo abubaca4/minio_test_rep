a4a381ea5cf125ac6762995052476eae1887084b
abubaca4 <abubaca4@gmail.com>
2021-03-09 16:54:22 +0300

    text format

--- minio_ecg/ecg_storage/models.py
+++ minio_ecg/ecg_storage/models.py
@@ -4,9 +4,12 @@ from django.core import validators
 
 # Create your models here.
 
+
 class source_org(models.Model):
-    name = models.CharField(max_length=200, unique=True, help_text="Название организации")
-    description = models.TextField(help_text="Дополнительная информация об организации", null=True, blank = True)
+    name = models.CharField(max_length=200, unique=True,
+                            help_text="Название организации")
+    description = models.TextField(
+        help_text="Дополнительная информация об организации", null=True, blank=True)
 
     class Meta:
         ordering = ["-name"]
@@ -14,53 +17,73 @@ class source_org(models.Model):
     def __str__(self):
         return str(self.name)
 
+
 class patients(models.Model):
-    sex_list = [('N', 'Не задан'), ('M','Мужской'), ('F', 'Женский')]
+    sex_list = [('N', 'Не задан'), ('M', 'Мужской'), ('F', 'Женский')]
     sex = models.CharField(
         max_length=1,
         choices=sex_list,
         default='N',
-	help_text="Пол пациента")
-    birthdate = models.DateField(help_text="Дата рождения", null=True, blank = True)
-    name = models.CharField(max_length=50, help_text="Имя", null=True, blank = True)
-    last_name = models.CharField(max_length=50, help_text="Фамилия", null=True, blank = True)
-    middle_name = models.CharField(max_length=50, help_text="Отчество", null=True, blank = True)
+        help_text="Пол пациента")
+    birthdate = models.DateField(
+        help_text="Дата рождения", null=True, blank=True)
+    name = models.CharField(
+        max_length=50, help_text="Имя", null=True, blank=True)
+    last_name = models.CharField(
+        max_length=50, help_text="Фамилия", null=True, blank=True)
+    middle_name = models.CharField(
+        max_length=50, help_text="Отчество", null=True, blank=True)
 
     class Meta:
         ordering = ["-birthdate"]
 
+
 class cardiac_pathology(models.Model):
-    name = models.CharField(max_length=100, unique=True, help_text="Название патологии")
-    short_name = models.CharField(max_length=20, help_text="Короткое название", null=True, blank = True)
+    name = models.CharField(max_length=100, unique=True,
+                            help_text="Название патологии")
+    short_name = models.CharField(
+        max_length=20, help_text="Короткое название", null=True, blank=True)
 
     class Meta:
         ordering = ["-name"]
 
     def __str__(self):
         return str(self.name)
+
 
 class access_groups(models.Model):
-    name = models.CharField(max_length=100, unique=True, help_text="Название группы")
-    description = models.TextField(help_text="Информация о группе доступа", null=True, blank = True)
+    name = models.CharField(max_length=100, unique=True,
+                            help_text="Название группы")
+    description = models.TextField(
+        help_text="Информация о группе доступа", null=True, blank=True)
 
     class Meta:
         ordering = ["-name"]
 
     def __str__(self):
         return str(self.name)
+
 
 class user_access(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     group_id = models.ForeignKey(access_groups, on_delete=models.CASCADE)
 
+
 class ecg(models.Model):
-    check_date = models.DateField(help_text="Дата снятия показаний", null=True, blank = True)
-    add_date = models.DateTimeField(help_text="Дата внесения в систему", auto_now_add=True)
-    patient_age = models.IntegerField(help_text="Возраст пациэнта на момент снятия показаний(для показаний не имеющих даты рождения пациента)", null=True, blank = True, validators=[validators.MinValueValidator(0), validators.MaxValueValidator(150)])
-    patient_id = models.ForeignKey(patients, models.SET_NULL, blank=True, null=True, help_text="Пациент которому принадлежит экг")
-    source_user = models.ForeignKey(User, models.SET_NULL, blank=True, null=True, help_text="Пользователь, загрузивший экг на сайт")
-    access_id = models.ForeignKey(access_groups, models.SET_NULL, blank=True, null=True, help_text="Группа доступа к которой принадлежит экг")
-    org_id = models.ForeignKey(source_org, models.SET_NULL, blank=True, null=True, help_text="Организация от которой получено экг")
+    check_date = models.DateField(
+        help_text="Дата снятия показаний", null=True, blank=True)
+    add_date = models.DateTimeField(
+        help_text="Дата внесения в систему", auto_now_add=True)
+    patient_age = models.IntegerField(help_text="Возраст пациэнта на момент снятия показаний(для показаний не имеющих даты рождения пациента)",
+                                      null=True, blank=True, validators=[validators.MinValueValidator(0), validators.MaxValueValidator(150)])
+    patient_id = models.ForeignKey(
+        patients, models.SET_NULL, blank=True, null=True, help_text="Пациент которому принадлежит экг")
+    source_user = models.ForeignKey(
+        User, models.SET_NULL, blank=True, null=True, help_text="Пользователь, загрузивший экг на сайт")
+    access_id = models.ForeignKey(access_groups, models.SET_NULL, blank=True,
+                                  null=True, help_text="Группа доступа к которой принадлежит экг")
+    org_id = models.ForeignKey(source_org, models.SET_NULL, blank=True,
+                               null=True, help_text="Организация от которой получено экг")
 
     class Meta:
         ordering = ["-add_date"]
@@ -68,10 +91,13 @@ class ecg(models.Model):
     def __str__(self):
         return str(self.add_date)
 
+
 class ecg_files(models.Model):
     ecg_id = models.ForeignKey(ecg, on_delete=models.CASCADE)
-    format = models.CharField(max_length=20, help_text="Тип файла", null=True, blank = True)
-    file_name = models.CharField(max_length=1024, unique=True, help_text="Имя объекта в MinIo")
+    format = models.CharField(
+        max_length=20, help_text="Тип файла", null=True, blank=True)
+    file_name = models.CharField(
+        max_length=1024, unique=True, help_text="Имя объекта в MinIo")
     sample_frequency = models.IntegerField()
     amplitude_resolution = models.IntegerField()
 
@@ -81,21 +107,26 @@ class ecg_files(models.Model):
     def __str__(self):
         return str(self.file_name)
 
+
 class original_information(models.Model):
     ecg_id = models.ForeignKey(ecg, on_delete=models.CASCADE)
     idMedServ = models.CharField(max_length=20)
     patientId = models.CharField(max_length=20)
     result = models.TextField()
 
+
 class ecg_tasks(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     ecg_id = models.ForeignKey(ecg, on_delete=models.CASCADE)
 
+
 class ecg_conclusion(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     ecg_id = models.ForeignKey(ecg, on_delete=models.CASCADE)
-    date_time = models.DateTimeField(help_text="Дата вынесения заключения", auto_now_add=True)
-    comment = models.TextField(help_text="Текстовое заключение", null=True, blank = True)
+    date_time = models.DateTimeField(
+        help_text="Дата вынесения заключения", auto_now_add=True)
+    comment = models.TextField(
+        help_text="Текстовое заключение", null=True, blank=True)
 
     class Meta:
         ordering = ["-date_time"]
@@ -103,6 +134,8 @@ class ecg_conclusion(models.Model):
     def __str__(self):
         return str(self.date_time)
 
+
 class conclusion_pathology(models.Model):
     conclusion_id = models.ForeignKey(ecg_conclusion, on_delete=models.CASCADE)
-    pathology_id = models.ForeignKey(cardiac_pathology, on_delete=models.CASCADE)
+    pathology_id = models.ForeignKey(
+        cardiac_pathology, on_delete=models.CASCADE)
--- minio_ecg/ecg_storage/views.py
+++ minio_ecg/ecg_storage/views.py
@@ -4,23 +4,26 @@ from .models import ecg
 
 # Create your views here.
 
+
 def index(request):
-     return render(
+    return render(
         request,
         'index.html',
         context={},
     )
 
+
 def add(request):
     return render(
         request,
-        'add.html',
+        'add_list.html',
         context={},
     )
+
 
 def add_ecg_file(request):
     return render(
         request,
-        'ecg_file.html',
+        'upload_file.html',
         context={'ecg_list': ecg.objects.all()},
     )
