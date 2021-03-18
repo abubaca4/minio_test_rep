2d8562f4847646d3f4899bd0e57ee476b95e34d5
abubaca4 <abubaca4@gmail.com>
2021-03-18 21:55:37 +0300

    ulr split

--- minio_ecg/ecg_storage/urls.py
+++ minio_ecg/ecg_storage/urls.py
@@ -7,16 +7,28 @@ from . import views
 urlpatterns = [
     path('', views.index, name='ecg_index'),
     path('add/', views.add_list, name='list_of_avaliable_models_addition'),
+    path('list/', views.common_list, name='list_models_records'),
+]
+
+urlpatterns += [
     re_path(r'^add/file/$', views.add_ecg_file, name='add_ecg_file_page'),
     path('add/patient/', views.add_patient, name='add_patient_page'),
     path('add/ecg/', views.add_ecg, name='add_ecg_page'),
-    path('list/', views.common_list, name='list_models_records'),
+]
+
+urlpatterns += [
     path('list/ecg/', views.ecg_list, name='list_ecg_records'),
+]
+
+urlpatterns += [
     path('view/file/<int:id>/', views.view_file, name='file_view'),
     path('view/file/<int:id>/download/',
          views.file_download_link, name='file_download'),
     path('view/ecg/<int:id>/', views.view_ecg, name='ecg_view'),
     path('view/patient/<int:id>/', views.view_patient, name='patient_view'),
+]
+
+urlpatterns += [
     path('edit/patient/<int:id>/', views.edit_patient, name='patient_edit'),
     path('edit/file/<int:id>/', views.edit_file, name='file_edit'),
 ]
