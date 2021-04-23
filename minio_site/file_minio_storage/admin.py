dbb8a776f3b2bf8e735efdc569854d26e37c8276
abubaca4 <abubaca4@gmail.com>
2021-04-23 14:32:55 +0300

    remove old code

--- minio_site/file_minio_storage/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from .models import File_uploaded
-
-admin.site.register(File_uploaded)
--- minio_site/file_minio_storage/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FileMinioStorageConfig(AppConfig):
-    name = 'file_minio_storage'
