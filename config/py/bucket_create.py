86d320ddcce8ce74040cb130389b3f1fef1d823f
abubaca4 <abubaca4@gmail.com>
2021-04-23 23:27:44 +0300

    fix 6

--- config/py/bucket_create.py
+++ config/py/bucket_create.py
@@ -1,7 +1,7 @@
 from minio import Minio
 
 client = Minio(
-    "s3:9000",
+    "83.149.198.179:32768",
     access_key="FAO5WMCSNYII9GNHMEVV6KX4",
     secret_key="rU7dkiJh4XN3dEoXeI9E2wdnIIUdZS9JuyFhG3F7r2UicIVU",
     secure=False,
